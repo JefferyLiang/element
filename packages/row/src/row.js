6b7fd72c1ebbee35abd29bdc66981229c453e2d2
Jeffery Liang <lzh.jeffery@gmail.com>
2018-07-06 11:13:55 +0800

    write the el-row component comment

--- packages/row/src/row.js
+++ packages/row/src/row.js
@@ -4,16 +4,21 @@ export default {
   componentName: 'ElRow',
 
   props: {
+    // 组件渲染的标签类型
     tag: {
       type: String,
       default: 'div'
     },
+    // 组件内部的列的分隔距离
     gutter: Number,
+    // 是有使用flex来行内布局
     type: String,
+    // 如果是flex布局的情况下 justify-content 的值
     justify: {
       type: String,
       default: 'start'
     },
+    // 如果是flex布局的情况下 align-items 的值
     align: {
       type: String,
       default: 'top'
@@ -23,7 +28,7 @@ export default {
   computed: {
     style() {
       const ret = {};
-
+      // 获取内部分隔的距离，去掉第一个和最后一个的影响
       if (this.gutter) {
         ret.marginLeft = `-${this.gutter / 2}px`;
         ret.marginRight = ret.marginLeft;
